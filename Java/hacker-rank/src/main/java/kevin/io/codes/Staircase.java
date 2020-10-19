36ed4a8f9276e4831c8614c1c783dd8cfd7b0cfa
1kevinson <kevinkouomeu@icloud.com>
2020-10-20 01:25:17 +0200

    Staircase algorithm fixed ⚖️

--- Java/hacker-rank/src/main/java/kevin/io/codes/Staircase.java
+++ Java/hacker-rank/src/main/java/kevin/io/codes/Staircase.java
@@ -3,9 +3,9 @@ package kevin.io.codes;
 public class Staircase {
 
     static void staircase(int n) {
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                if ((i + j) > n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if ((i + j) >= n - 1) {
                     System.out.print("#");
                 } else {
                     System.out.print(" ");
@@ -19,8 +19,8 @@ public class Staircase {
 
 // Think of 2D Array
 
-//         1 2 3 4
-//      1 [ , , ,#]
-//      2 [ , ,#,#]
-//      3 [ ,#,#,#]
-//      4 [#,#,#,#]
+//         0 1 2 3
+//      0 [ , , ,#]
+//      1 [ , ,#,#]
+//      2 [ ,#,#,#]
+//      3 [#,#,#,#]
