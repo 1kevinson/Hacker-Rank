c25d46773c743304b0487ef0097c64b95a51d592
Arsene Kevin <kevinkouomeu@icloud.com>
2020-12-29 02:16:32 +0100

    [Conway] - Conway Suite

--- Java/hacker-rank/src/test/java/kevin/io/codes/ConwaySuiteTest.java
+++ Java/hacker-rank/src/test/java/kevin/io/codes/ConwaySuiteTest.java
@@ -1,7 +1,24 @@
 package kevin.io.codes;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConwaySuiteTest {
+
+    @Test
+    void shouldDrawNextLineOfConwaySuite() {
+        expectedConwayLines("1", "1", "1 1");
+        expectedConwayLines("2", "2", "1 2");
+        expectedConwayLines("2 2", "2 2", "2 2");
+        expectedConwayLines("3 3", "3 3", "2 3");
+        expectedConwayLines("2 1", "2 1", "1 2 1 1");
+        expectedConwayLines("2 1 3 3", "2 1 3 3", "1 2 1 1 2 3");
+        expectedConwayLines("2 1 3 1", "2 1 3 1", "1 2 1 1 1 3 1 1");
+    }
+
+    public void expectedConwayLines(String line, String... expectedLines) {
+        assertEquals(String.join("\n", expectedLines), new ConwaySuite().draw(line));
+    }
 
 }
