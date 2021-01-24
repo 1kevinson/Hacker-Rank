d2e4595efbee61a0f3eece4430f1ce062ce03c0f
1kevinson <kevinkouomeu@icloud.com>
2021-01-24 21:27:09 +0100

    Change dir for cucumber

--- Java/hacker-rank/src/main/java/kevin/io/learn/Code.java
+++ Java/hacker-rank/src/main/java/kevin/io/learn/Code.java
@@ -1,8 +1,17 @@
 package kevin.io.learn;
 
+import java.util.Arrays;
+
 public class Code {
 
     public static void main(String[] args) {
-
+        String[] test = {"eat", "food", "everyday", "haha"};
+        Byte[] bytes = {10, 20, 15, 22, 35};
+        System.out.println(Arrays.binarySearch(bytes, (byte) 22));
     }
+}
+
+
+class CommonClass {
+
 }
--- Java/hacker-rank/src/test/java/RunCucumberTest.java
+++ Java/hacker-rank/src/test/java/RunCucumberTest.java
@@ -1,5 +1,3 @@
-package kevin.io.codes.hacker.gene;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
