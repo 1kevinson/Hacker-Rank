4074305d1b23002710ddbaec2e81e4fe2def55bf
Arsene Kevin <kevinkouomeu@icloud.com>
2021-01-25 00:00:56 +0100

    code test

--- Java/hacker-rank/src/main/java/kevin/io/learn/Code.java
+++ Java/hacker-rank/src/main/java/kevin/io/learn/Code.java
@@ -5,8 +5,18 @@ import java.util.Arrays;
 public class Code {
 
     public static void main(String[] args) {
-        Bike bike1 = new Bike();
-        Bike bike2 = new Bike();
+        final Character[][] arrays = new Character[4][5];
+        Arrays.stream(arrays).forEach(arrayRow -> Arrays.fill(arrayRow, '.'));
+
+        arrays[2][3] = 'M';
+        arrays[1][0] = 'V';
+        arrays[2][1] = 'M';
+
+        for (Character[] array : arrays) {
+            System.out.println(Arrays.toString(array));
+        }
+
+        new Bike().notify();
     }
 }
 
@@ -14,12 +24,11 @@ public class Code {
 class Bike {
 
     private static int count = 1;
+    static int[][] arr = new int[3][3];
 
     public Bike() {
-        System.out.println("constructor invoked" + count ++);
+        System.out.println("constructor invoked" + count++);
     }
 
-    static {
-        System.out.println("Instance initializer invoked");
-    }
+
 }
--- Java/hacker-rank/src/test/java/kevin/io/codes/hacker/gene/StepDefinitions.java
+++ Java/hacker-rank/src/test/java/kevin/io/codes/hacker/gene/StepDefinitions.java
@@ -15,16 +15,19 @@ public class StepDefinitions {
         this.gene = new Gene(geneSequence);
     }
 
+
     @When("performs an ajustement to fix it to steady")
     public void performs_an_ajustement_to_fix_it_to_steady() throws Exception {
         final GeneManager manager = new GeneManager();
         actualAnswer = manager.findTheSmallestPossibleStringToModify(gene);
     }
 
+
     @Then("get the minimal sub-string to change is {int}")
     public void get_the_minimal_sub_string_to_change_is(Integer expectedNumber) {
         Assertions.assertEquals(expectedNumber, actualAnswer);
     }
+
 
     @Then("get {int} as the minimal sub-substring to change")
     public void get_as_the_minimal_sub_substring_to_change(Integer expectedNumber) {
