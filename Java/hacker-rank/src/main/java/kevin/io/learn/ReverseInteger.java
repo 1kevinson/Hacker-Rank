637825966e1c722b2881a872dc551691fb73f7c1
Arsene Kevin KOUOMEU <59510116+1kevinson@users.noreply.github.com>
2021-02-05 23:24:39 +0100

    Leet Code Challenge - ReverseInteger update

--- Java/hacker-rank/src/main/java/kevin/io/learn/ReverseInteger.java
+++ Java/hacker-rank/src/main/java/kevin/io/learn/ReverseInteger.java
@@ -4,21 +4,26 @@ import org.junit.jupiter.api.Test;
 
 import static java.lang.Integer.parseInt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReverseInteger {
 
     public static int go(int number) {
 
-        String reversedString = new StringBuilder()
-                                        .append(Math.abs(number))
-                                        .reverse()
-                                        .toString();
+        if(!isFollowConstraint(number)) throw new NumberFormatException();
 
-        try {
+        String reversedString = new StringBuilder()
+                .append(Math.abs(number))
+                .reverse()
+                .toString();
+
+
             return (number < 0) ? -1 * parseInt(reversedString) : parseInt(reversedString);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
+
+    }
+
+    private static boolean isFollowConstraint(int number){
+        return number >= -Math.pow(2,31) && number <= Math.pow(2,31);
     }
 
 }
@@ -49,4 +54,11 @@ class ReverseTest {
     void shouldReturnCorrectReverseForNumberWith0AtTheEnd() {
         assertEquals(54, ReverseInteger.go(450));
     }
+
+    @Test
+    void shouldVerifyTheConstraint() {
+        assertThrows(NumberFormatException.class, () -> ReverseInteger.go((int) Math.pow(2, 40)));
+    }
+
+
 }
