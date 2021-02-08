29b2afb554d9220a3b8251b95c414ce90c01cdd8
Arsene Kevin KOUOMEU <59510116+1kevinson@users.noreply.github.com>
2021-02-08 23:07:39 +0100

    Leet Code Challenge - Remove roman to literal

--- Java/hacker-rank/src/main/java/kevin/io/learn/LeetCode.java
+++ Java/hacker-rank/src/main/java/kevin/io/learn/LeetCode.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -218,103 +217,4 @@ class PalindromeNumber {
     }
 }
 
-class RomanNumeral {
-
-/*
-
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II.
-The number 27 is written as XXVII, which is XX + V + II.
-
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
-Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
-The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-I can be placed before V (5) and X (10) to make 4 and 9.
-X can be placed before L (50) and C (100) to make 40 and 90.
-C can be placed before D (500) and M (1000) to make 400 and 900.
-Given a roman numeral, convert it to an integer.
-
-*
-* */
-
-    public static int romanToInt(String roman) {
-        if (!isRomanValid(roman)) return -1;
-
-        if (roman.length() < 2) return getIntFromSingleRoman(roman);
-
-        final String[] romanSplit = roman.split("");
-        final List<String> romanSplitList = new ArrayList<>(Arrays.asList(romanSplit));
-
-        return getListOfNumbers(romanSplitList).stream().reduce(0, Integer::sum);
-    }
-
-    public static List<Integer> transformRomanWithConditions(List<String> stringList) {
-        final List<Integer> newListReduced = new ArrayList<>();
-        final List<Integer> oldListReduced = new ArrayList<>();
-
-        for (int i = 0; i < stringList.size(); i++) {
-
-        }
-
-        return newListReduced;
-    }
-
-    public static boolean isRomanValid(String romanString) {
-        return Pattern.matches("[IVXLCDM]+", romanString);
-    }
-
-    public static List<Integer> getListOfNumbers(List<String> stringList) {
-        return stringList.stream().map(RomanNumeral::getIntFromSingleRoman).collect(Collectors.toList());
-    }
-
-    public static int getIntFromSingleRoman(String character) {
-        switch (character) {
-            case "I":
-                return 1;
-            case "V":
-                return 5;
-            case "X":
-                return 10;
-            case "L":
-                return 50;
-            case "C":
-                return 100;
-            case "D":
-                return 500;
-            case "M":
-                return 1000;
-            default:
-                return -1;
-        }
-    }
-
-    @DisplayName("Test Roman Numeral Numbers")
-    static class Tests {
-
-        @Test
-        void shouldReturnAInvalidResponse() {
-            assertEquals(-1, RomanNumeral.romanToInt("IITR"));
-        }
-
-        @Test
-        void shouldReturn1() {
-            assertEquals(1, RomanNumeral.romanToInt("I"));
-        }
-
-        @Test
-        void shouldReturn2() {
-            assertEquals(2, RomanNumeral.romanToInt("II"));
-        }
-
-        @Test
-        void shouldReturn3() {
-            assertEquals(3, RomanNumeral.romanToInt("III"));
-        }
-
-        @Test
-        void shouldReturn4() {
-            assertEquals(4, RomanNumeral.romanToInt("IV"));
-        }
-    }
-}
 
