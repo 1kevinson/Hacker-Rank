24a36111ddc87968677b35a8be9f93647e3d6cb2
Arsene Kevin KOUOMEU <59510116+1kevinson@users.noreply.github.com>
2021-02-09 21:14:44 +0100

    Formation UML - Add class Person + correct

--- Java/hacker-rank/src/main/java/kevin/io/formations/uml/Person.java
+++ Java/hacker-rank/src/main/java/kevin/io/formations/uml/Person.java
@@ -7,18 +7,17 @@ public class Person {
     private String address;
     private int age;
 
-    public boolean isNameLengthCorrect() {
-        return this.name.trim().length() >= 5;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() throws Exception {
         if (!isNameLengthCorrect()) throw new Exception("Name Length is not correct");
 
         return this.name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isNameLengthCorrect() {
+        return this.name.trim().length() >= 5;
+    }
 }
