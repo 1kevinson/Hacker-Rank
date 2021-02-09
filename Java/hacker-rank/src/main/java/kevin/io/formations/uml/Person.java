48762eb0e38f79b022b22e4d8d5f912092ba723c
Arsene Kevin KOUOMEU <59510116+1kevinson@users.noreply.github.com>
2021-02-09 22:46:19 +0100

    Formation UML - Add class Schedule and Meeting

--- Java/hacker-rank/src/main/java/kevin/io/formations/uml/Person.java
+++ Java/hacker-rank/src/main/java/kevin/io/formations/uml/Person.java
@@ -1,6 +1,9 @@
 package kevin.io.formations.uml;
 
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Person {
 
     private String name;
@@ -19,5 +22,34 @@ public class Person {
 
     public boolean isNameLengthCorrect() {
         return this.name.trim().length() >= 5;
+    }
+
+
+}
+
+class Schedule {
+
+    private final String name;
+    private List<Meeting> scheduledMeeting;
+
+    public Schedule(String name) {
+        this.name = name;
+    }
+
+    public void addMeetingInSchedule(Meeting newMeeting) {
+        this.scheduledMeeting.add(newMeeting);
+    }
+}
+
+class Meeting {
+
+    private final String description;
+    private final LocalDate dateStart;
+    private final LocalDate dateEnd;
+
+    Meeting(String description, LocalDate dateStart, LocalDate dateEnd) {
+        this.description = description;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
     }
 }
